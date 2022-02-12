9921ac1d4d845cc70478bc0d33c0a14bb20bdd0b
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-12 16:38:40 +0100

    Create Solution Compare Nodes.js

---
+++ Solution Compare Nodes.js
@@ -0,0 +1,51 @@
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function (root, p) {
+    this.p_successorInOrder = null;
+    this.previous = null;
+    return (p.right !== null) ? leftmostNodeFromCurrentRoot(p.right) : inorder(root, p);
+};
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+function inorder(root, p) {
+
+    if (root === null) {
+        return null;
+    }
+    if (this.p_successorInOrder !== null) {
+        return this.p_successorInOrder;
+    }
+
+    inorder(root.left, p);
+    if (this.previous === p) {
+        this.p_successorInOrder = root;
+    }
+    this.previous = root;
+    inorder(root.right, p);
+
+    return this.p_successorInOrder;
+}
+
+/**
+ * @param {TreeNode} node
+ * @return {TreeNode}
+ */
+function leftmostNodeFromCurrentRoot(node) {
+    while (node !== null && node.left !== null) {
+        node = node.left;
+    }
+    return node;
+}
