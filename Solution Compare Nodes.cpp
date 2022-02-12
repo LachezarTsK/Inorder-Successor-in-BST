2dcda48733c0347af600e4b777f927a0b0ad601f
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-02-12 16:41:55 +0100

    Create Solution Compare Nodes.cpp

---
+++ Solution Compare Nodes.cpp
@@ -0,0 +1,45 @@
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+    
+public:
+    TreeNode* p_successorInOrder{};
+    TreeNode* previous{};
+
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        return (p->right != nullptr) ? leftmostNodeFromCurrentRoot(p->right) : inorder(root, p);
+    }
+
+    TreeNode* inorder(TreeNode* root, TreeNode* p) {
+
+        if (root == nullptr) {
+            return nullptr;
+        }
+        if (p_successorInOrder != nullptr) {
+            return p_successorInOrder;
+        }
+
+        inorder(root->left, p);
+        if (previous == p) {
+            p_successorInOrder = root;
+        }
+        previous = root;
+        inorder(root->right, p);
+
+        return p_successorInOrder;
+    }
+
+    TreeNode* leftmostNodeFromCurrentRoot(TreeNode* node) {
+        while (node != nullptr && node->left != nullptr) {
+            node = node->left;
+        }
+        return node;
+    }
+};
